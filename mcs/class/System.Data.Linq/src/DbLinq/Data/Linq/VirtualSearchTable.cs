3eb5a005903368edaf6f8cd2452701488dc720cf
Andrea Martinelli <martinelli-andrea@outlook.com>
2014-10-04 17:45:02 +0200

    VirtualSearchTable

---
+++ mcs/class/System.Data.Linq/src/DbLinq/Data/Linq/VirtualSearchTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbLinq.Data.Linq
+{
+    public class VirtualSearchTable
+    {
+        public string DummyField;
+    }
+}
