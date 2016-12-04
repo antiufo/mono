298161eb8df128ca0398d95d86622c6b464b8331
Andrea Martinelli <andmartinelli@gmail.com>
2016-12-04 20:44:59 +0100

    Fixed useless casts

--- mcs/class/System.Data.Linq/src/DbLinq/Data/Linq/DataContext.cs
+++ mcs/class/System.Data.Linq/src/DbLinq/Data/Linq/DataContext.cs
@@ -79,7 +79,7 @@ namespace DbLinq.Data.Linq
         public MetaModel Mapping { get; private set; }
         // PC question: at ctor, we get a DbConnection and the Connection property exposes a DbConnection
         //              WTF?
-        public DbConnection Connection { get { return DatabaseContext.Connection as DbConnection; } }
+        public DbConnection Connection { get { return DatabaseContext.Connection } }
 
 
         public IQueryable<VirtualSearchTable> GetFullTextSearchTable(string language, string searchTerms, int searchQueryId)
@@ -1337,7 +1337,7 @@ namespace DbLinq.Data.Linq
         [DbLinqToDo]
         public DbCommand GetCommand(IQueryable query)
         {
-            DbCommand dbCommand = GetDbCommand(query) as DbCommand;
+            var dbCommand = GetDbCommand(query);
             if (dbCommand == null)
                 throw new InvalidOperationException();
 
